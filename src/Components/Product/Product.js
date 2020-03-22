f34cfceb2110643cb3a89aa6b760bf947aba844a
tanbirsakib <you@example.com>
2020-03-22 17:14:09 +0600

    added product info

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Product'
+import './Product';
+import './product.css';
 const Product = (props) => {
-    console.log(props.product.name);
+    const{name, img, seller, price, stock} = props.product;
+    console.log(props.product);
     return (
+        <div className='product'>
         <div>
-            {props.product.name}
+        <img src={img} alt=""/>
+        </div>
+        <div className='product-info'>
+        <h4 className='product-name'>{name}</h4>
+        <br/>
+        <p><small>Powered by: {seller}</small></p>
+        <p><small>${price}</small></p>
+        <p>Only available : {stock} Do hurry to get one.</p>
+        </div>
+            
         </div>
     );
 };
